b9627a89163b8a54f32308435eded2ddb4d0d98e
Kazuichi Takashiro <takashiro@qq.com>
2021-02-02 19:54:54 +0800

    Add build command

--- index.ts
+++
@@ -1,5 +0,0 @@
-export { default as Action } from './lib/Action';
-export { default as Driver } from './lib/Driver';
-export { default as Room } from './lib/Room';
-export { default as User } from './lib/User';
-export { default as UserProfile } from './lib/UserProfile';
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { default as Action } from './Action';
+export { default as Driver } from './Driver';
+export { default as Room } from './Room';
+export { default as User } from './User';
+export { default as UserProfile } from './UserProfile';
