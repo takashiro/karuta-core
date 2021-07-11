33aa99c5141a9c43dcb7298d8e8befc8785eb99e
Kazuichi Takashiro <takashiro@qq.com>
2021-07-11 15:07:03 +0800

    Add network functions

--- src/User.ts
+++ src/User.ts
@@ -1,3 +1,5 @@
+import { Method } from '@karuta/protocol';
+
 import Room from './Room';
 import Driver from './Driver';
 
@@ -18,6 +20,12 @@ interface User {
 	getName(): string | undefined;
 
 	/**
+	 * Sets user name
+	 * @param name
+	 */
+	setName(name: string): void;
+
+	/**
 	 * Gets the current room
 	 */
 	getRoom(): Room | undefined;
@@ -33,9 +41,83 @@ interface User {
 	isConnected(): boolean;
 
 	/**
+	 * Log out the user.
+	 */
+	logout(): void;
+
+	/**
 	 * @return brief introduction to the user
 	 */
 	getProfile(): UserProfile;
+
+
+	/**
+	 * Sets maximum time limit for each request.
+	 * @param timeout
+	 */
+	setRequestTimeout(timeout?: number): void;
+
+	/**
+	 * @returns Maximum time limit for a request.
+	 */
+	getRequestTimeout(): number | undefined;
+
+	/**
+	 * Get something from the user.
+	 * @param context
+	 * @param params
+	 */
+	get(context: number, params?: unknown): Promise<unknown>;
+
+	/**
+	 * Get meta information of something from the user.
+	 * @param context
+	 * @param params
+	 */
+	head(context: number, params?: unknown): Promise<unknown>;
+
+	/**
+	 * Post changes to the user.
+	 * @param context
+	 * @param params
+	 */
+	post(context: number, params?: unknown): Promise<unknown>;
+
+	/**
+	 * Put something on the user.
+	 * @param context
+	 * @param params
+	 */
+	put(context: number, params?: unknown): Promise<unknown>;
+
+	/**
+	 * Patch something on the user.
+	 * @param context
+	 * @param params
+	 */
+	patch(context: number, params?: unknown): Promise<unknown>;
+
+	/**
+	 * Delete something from the user.
+	 * @param context
+	 * @param params
+	 */
+	delete(context: number, params?: unknown): Promise<unknown>;
+
+	/**
+	 * Send a request
+	 * @param method
+	 * @param context
+	 * @param params
+	 */
+	request(method: Method, context: number, params?: unknown): Promise<unknown>;
+
+	/**
+	 * Send a one-way notification to the user.
+	 * @param context
+	 * @param params
+	 */
+	notify(method: Method, context: number, params?: unknown): void;
 }
 
 export default User;
